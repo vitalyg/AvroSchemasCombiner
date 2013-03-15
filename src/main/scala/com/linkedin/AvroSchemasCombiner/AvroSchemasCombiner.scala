d95ef50baee1b269c8869420f79dc178e8a925a8
Vitaly Gordon <vgordon@vgordon-mn.(none)>
2013-03-15 23:13:24 +0200

    Initial Commit

---
+++ src/main/scala/com/linkedin/AvroSchemasCombiner/AvroSchemasCombiner.scala
@@ -0,0 +1,100 @@
+package com.linkedin.AvroSchemasCombiner
+
+
+import java.io.{FileWriter, BufferedWriter, File, FileFilter}
+import io.Source
+import play.api.libs.json._
+import annotation.tailrec
+
+
+/**
+ * User: vgordon
+ * Date: 1/8/13
+ * Time: 10:07 AM
+ */
+object AvroSchemasCombiner {
+  val avroSchemaExtension = "avsc"
+
+  def getSchemaName(schema: JsObject) : String = (schema \ "name").as[String]
+
+  def getSchema(file: File) : (String, JsObject) = {
+    try {
+      val schema = Json.parse(Source.fromFile(file).mkString).as[JsObject]
+      (getSchemaName(schema), schema)
+    }
+    catch {
+      case e: Exception => throw new Exception("Bad Avro Schema\n" + file)
+    }
+  }
+
+  def getAvroSchemas(file: File) : Map[String, JsObject] = {
+    val avroFilesFilter = new FileFilter {
+      def accept(file: File) = file.isDirectory || file.getName.endsWith(avroSchemaExtension)
+    }
+
+    val avroFilesAndDirs = file.listFiles(avroFilesFilter)
+    val (dirs, files) = avroFilesAndDirs.partition(_.isDirectory)
+    dirs.map(getAvroSchemas).flatten.toMap ++ files.map(getSchema).toMap
+  }
+
+  def getDependencyGraph(schemas: Map[String, JsObject]) : Map[String, Set[String]] = {
+    def getSchemaDependencies(schema: JsValue) : Set[String] = AvroSchemasUtils.getDependencies(schema).map(_.as[String]).toSet
+
+    schemas.mapValues(schema => getSchemaDependencies(schema))
+  }
+
+  @tailrec
+  def topologicalSort(graph: Map[String, Set[String]], sortedGraph: List[String] = List()) : List[String] = {
+    if (graph.isEmpty)
+      return sortedGraph.distinct.reverse
+
+    val node = graph.find(_._2.isEmpty)
+    node match {
+      case Some(schema) => {
+        val key = schema._1
+        val newGraph = (graph - key).mapValues(depends => depends - key)
+        topologicalSort(newGraph, key :: sortedGraph)
+      }
+      case None => throw new Exception("Circular dependency in schema")
+    }
+  }
+
+  def getSchemasInOrder(schemas: Map[String, JsObject], order: Seq[String]) : JsArray = {
+    Json.toJson(order.map(schemaName => schemas(schemaName))).as[JsArray]
+  }
+
+
+  //implicit def toJsonArray(schemas: Seq[JsValue]) = JsArray(schemas)
+
+  def main(args: Array[String]) {
+    val avroDirectory = args(0)
+    val outputPath = args(1)
+
+    val inputDir = new File(avroDirectory)
+    val outputFile = new File(outputPath)
+
+    if (!outputFile.exists)
+      outputFile.createNewFile
+
+    val schemas = getAvroSchemas(inputDir)
+    val namespacedSchemas = schemas.values.map(x => AvroSchemasUtils.namespaceSchema(x))
+    val nestedSchemas = namespacedSchemas.flatMap(x => AvroSchemasUtils.getNestedSchemas(x))
+    val flatSchemas = nestedSchemas.map(x => (AvroSchemasUtils.getNameWithNamespace(x.as[JsObject]), AvroSchemasUtils.flattenSchema(x.as[JsObject]))).toMap
+    val order = topologicalSort(getDependencyGraph(flatSchemas))
+
+    val bw = new BufferedWriter(new FileWriter(outputFile))
+    bw.write(Json.stringify(getSchemasInOrder(flatSchemas, order)))
+    bw.close()
+
+//    val z = AvroSchemasUtils.namespaceSchema(schemas("InmailSendEvent"))
+//    println(z)
+//    val a = AvroSchemasUtils.getNestedSchemas(z)
+//    println(a)
+//    val b = a.map(x => (AvroSchemasUtils.getNameWithNamespace(x.as[JsObject]), AvroSchemasUtils.flattenSchema(x.as[JsObject]))).toMap
+//    println(b)
+//    val c = getDependencyGraph(b)
+//    println(c)
+//    val d = topologicalSort(c)
+//    println(d)
+  }
+}
---
+++ src/main/scala/com/linkedin/AvroSchemasCombiner/AvroSchemasUtils.scala
@@ -0,0 +1,177 @@
+package com.linkedin.AvroSchemasCombiner
+
+
+import play.api.libs.json._
+
+/**
+ * User: vgordon
+ * Date: 1/11/13
+ * Time: 1:51 PM
+ */
+trait AvroTraversal {
+  def recordInitialValue(schema: JsValue): Option[JsValue] = None
+  def traverseEnumType(schema: JsValue): Seq[JsValue] = Seq()
+  def traverseFixedType(schema: JsValue): Seq[JsValue] = Seq()
+  def traversePrimitiveType(schema: JsValue): Seq[JsValue] = Seq()
+  def traverseReferenceType(schema: JsValue): Seq[JsValue] = Seq()
+}
+
+object InternallyDefinedSchemasExtractor extends AvroTraversal {
+  override def recordInitialValue(schema: JsValue) = Some(schema)
+  override def traverseEnumType(schema: JsValue) = Seq(schema)
+  override def traverseFixedType(schema: JsValue) = Seq(schema)
+}
+
+object DependenciesExtractor extends AvroTraversal {
+  override def traverseReferenceType(schema: JsValue) = Seq(schema)
+}
+
+object AvroSchemasUtils {
+  val Primitive = "(null|boolean|int|long|float|double|bytes|string)".r
+
+  def getNameWithNamespace(schema: JsObject) : String = {
+    val namespace = (schema \ "namespace").as[String]
+    val name = (schema \ "name").as[String]
+    getNameWithNamespace(namespace, name)
+  }
+
+  def getNameWithNamespace(namespace: String, name: String) : String = {
+    if (name.contains("."))
+      name
+    else
+      namespace + "." + name
+  }
+
+  def traverseSchema(schema: JsValue, strategy: AvroTraversal) : Seq[JsValue] = {
+    def parseByType(schema: JsValue) : Seq[JsValue] = {
+      val schemaType = (schema \ "type")
+      schemaType match {
+        // union definition
+        case JsArray(types) => types.flatMap(element => traverseSchema(element, strategy))
+        // built-in type
+        case JsString(str) => parseString(str)
+        // type definition
+        case JsObject(_) => traverseSchema(schemaType, strategy)
+        case _ => throw new Exception("Unrecognizable JSON element\n" + schema)
+      }
+    }
+
+    def parseString(schemaType: String) : Seq[JsValue] = {
+      schemaType match {
+        case "record" => {
+          val recordFields = (schema \ "fields").as[JsArray].value
+          strategy.recordInitialValue(schema) match {
+            case Some(value) => value +: recordFields.flatMap(field => traverseSchema(field, strategy))
+            case None => recordFields.flatMap(field => traverseSchema(field, strategy))
+          }
+        }
+        case "array" => traverseSchema(schema \ "items", strategy)
+        case "map" => traverseSchema(schema \ "values", strategy)
+        case "enum" => strategy.traverseEnumType(schema)
+        case "fixed" => strategy.traverseFixedType(schema)
+        case Primitive(_) => strategy.traversePrimitiveType(schema)
+        case _ => strategy.traverseReferenceType(JsString(schemaType)) // class reference
+      }
+    }
+
+    schema match {
+      case JsArray(elements) => elements.flatMap(el => traverseSchema(el, strategy))
+      case JsObject(_) => parseByType(schema)
+      case JsString(str) => parseString(str)
+      case _ => throw new Exception("Unrecognizable JSON element\n" + schema)
+    }
+  }
+
+  def flattenSchema(schema: JsObject) : JsObject = {
+    def modifySchemaElement(schema: JsObject, key: String, newVal: JsValue) = schema - key + (key -> newVal)
+    def modifySchema(schema: JsValue) : JsValue = {
+      def parseByType(schema: JsObject) : JsValue = {
+        val schemaType = schema \ "type"
+        schemaType match {
+          // union definition
+          case JsArray(types) => modifySchemaElement(schema, "type", JsArray(types.map(element => modifySchema(element))))
+          // built-in type
+          case JsString(str) => {
+            str match {
+              case "record" => JsString(getNameWithNamespace(schema))
+              case "array" => modifySchemaElement(schema, "items", modifySchema(schema \ "items"))
+              case "map" => modifySchemaElement(schema, "values", modifySchema(schema \ "values"))
+              case "enum" => JsString(getNameWithNamespace(schema))
+              case "fixed" => JsString(getNameWithNamespace(schema))
+              case Primitive(_) => schema
+              case _ => schema // class reference
+            }
+          }
+          // type definition
+          case JsObject(_) => modifySchemaElement(schema, "type", modifySchema(schemaType))
+          case _ => throw new Exception("Unrecognizable JSON element\n" + schema)
+        }
+      }
+
+      schema match {
+        case JsArray(elements) => JsArray(elements.map(modifySchema))
+        case JsObject(_) => parseByType(schema.as[JsObject])
+        case JsString(str) => schema
+        case _ => throw new Exception("Unrecognizable JSON element\n" + schema)
+      }
+    }
+
+    val schemaType = (schema \ "type").as[String]
+    schemaType match {
+      case "record" => modifySchemaElement(schema, "fields", modifySchema(schema \ "fields"))
+      case "enum" => schema
+      case "fixed" => schema
+      case _ => throw new Exception("Unrecognizable JSON element\n" + schema)
+    }
+  }
+
+  def namespaceSchema(schema: JsObject) : JsObject = {
+    def modifySchema(schema: JsValue, namespace:String) : JsValue = {
+      def modifySchemaElement(schema: JsValue, key: String, newVal: JsValue) = schema.as[JsObject] - key + (key -> newVal)
+      def addNamespace(schema: JsValue) = schema.as[JsObject] + ("namespace" -> JsString(namespace))
+      def parseByType(schema: JsValue) : JsValue = {
+        val schemaType = schema \ "type"
+        schemaType match {
+          // union definition
+          case JsArray(types) => modifySchemaElement(schema, "type", JsArray(types.map(element => modifySchema(element, namespace))))
+          // built-in type
+          case JsString(str) => parseStringType(str)
+          // type definition
+          case JsObject(_) => modifySchemaElement(schema, "type", modifySchema(schemaType, namespace))
+          case _ => throw new Exception("Unrecognizable JSON element\n" + schema)
+        }
+      }
+
+      def parseStringType(schemaType: String) : JsValue = {
+        schemaType match {
+          case "record" => addNamespace(modifySchemaElement(schema, "fields", modifySchema(schema \ "fields", namespace)))
+          case "array" => modifySchemaElement(schema, "items", modifySchema(schema \ "items", namespace))
+          case "map" => modifySchemaElement(schema, "values", modifySchema(schema \ "values", namespace))
+          case "enum" => addNamespace(schema)
+          case "fixed" => addNamespace(schema)
+          case Primitive(_) => schema
+          case _ => modifySchemaElement(schema, "type", JsString(getNameWithNamespace(namespace, schemaType))) // class reference
+        }
+      }
+
+      def parseString(str: String) : JsValue = {
+        str match {
+          case Primitive(_) => schema
+          case _ => JsString(getNameWithNamespace(namespace, str)) // class reference
+        }
+      }
+
+      schema match {
+        case JsArray(elements) => JsArray(elements.map(element => modifySchema(element, namespace)))
+        case JsObject(_) => parseByType(schema)
+        case JsString(str) => parseString(str)
+        case _ => throw new Exception("Unrecognizable JSON element\n" + schema)
+      }
+    }
+
+    modifySchema(schema, (schema \ "namespace").as[String]).as[JsObject]
+  }
+
+  def getNestedSchemas(schema: JsValue) = traverseSchema(schema, InternallyDefinedSchemasExtractor)
+  def getDependencies(schema: JsValue) = traverseSchema(schema, DependenciesExtractor).diff(Seq(JsString(getNameWithNamespace(schema.as[JsObject]))))
+}
